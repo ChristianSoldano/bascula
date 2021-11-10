908f05b2f0d367339769c0b84665ddae8b8a65aa
ChristianSoldano <soldanochristian@hotmail.com>
2021-11-10 00:37:36 -0300

    fix variable host

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [config('DEBUG'), '127.0.0.1']
+ALLOWED_HOSTS = [config('HOST_URL'), '127.0.0.1']
 
 
 # Application definition
